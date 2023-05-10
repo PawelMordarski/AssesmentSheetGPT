cfbc49182f9e18e35f3c6b2f1e069a0dffcfd8c0
PawelMordarski <pmordarski@outlook.com>
2023-05-10 19:23:22 +0200

    1 commit - create project, properties, pom etc.

---
+++ src/main/java/pl/coderslab/assesmentsheetgpt/AssesmentSheetGptApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.assesmentsheetgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssesmentSheetGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssesmentSheetGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/assesmentsheetgpt/AssesmentSheetGptApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.assesmentsheetgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmentSheetGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
